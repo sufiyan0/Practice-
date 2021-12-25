f0ac20783587550c54507619ba35c6da099462c3
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2021-12-25 14:59:58 +0500

    Revert "jawascript"
    This reverts commit e9ed20cac819c86cba618e7212b68c84d7622e5c.

--- sign in/script.js
+++ sign in/script.js
@@ -18,38 +18,34 @@ function showSuccess(input){
 };
 
 
-function validEmail(email){
-    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email).toLowerCase());
-};
-
-function checkRequired(inpurArray) {
-
-    inpurArray.forEach(function(input) {
-
-        if(input.value === ''){
-            showError(input,`${getFieldId(input)} is required`);
-        }else{
-            showSuccess(input);
-        }
-    });
-}
-
-function getFieldId(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-
-function checkLength(input, min, max,) {
-    
-}
-
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     // console.log("sufiyan");
 
-    checkRequired([uname,email,password,password1]);
-    checkLength(uname,3,10);
-    checkLength(password,6,30);
+    if (uname.value === ''){
+        showError(uname , "UserName is  Required");
+    }else{
+        showSuccess(uname);
+    }
+
+    if (email.value === ''){
+        showError(email , "Email is Required");
+    }else{
+        showSuccess(email);
+    }
+
+    if (password.value === ''){
+        showError(password , "Password is Required");
+    }else{
+        showSuccess(password);
+    }
+
+
+        if (password1.value === ''){
+            showError(password1 , "confirm Your password");
+        }else{
+            showSuccess(password1);
+
+        }
 })
