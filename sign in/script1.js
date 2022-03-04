1b934e2c87adf8637b830d45e3fa5d7f49b03c5d
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2022-03-04 16:00:38 +0500

    00
    00

--- sign in/script1.js
+++ sign in/script1.js
@@ -4,6 +4,43 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password1 = document.getElementById("password1");
 
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'content success';
+}
+
+function showError(input,massage){
+    const formControl = input.parentElement;
+    formControl.className = 'content error';
+    const small = formControl.querySelector('small')
+    small.innerText = massage;
+}
+
+ 
 forn.addEventListener("submit", function(e){
     e.preventDefault();
+
+    if(username.value === ''){
+        showError(username,"required") 
+    } else {
+        showSuccess(username);
+    };
+
+    if(email.value === ''){
+        showError(email,"required") 
+    } else {
+        showSuccess(email);
+    };
+
+    if(password.value === ''){
+        showError(password,"required") 
+    } else {
+        showSuccess(password);
+    };
+    
+    if(password1.value === ''){
+        showError(password1,"required") 
+    } else {
+        showSuccess(password1);
+    };
 })
